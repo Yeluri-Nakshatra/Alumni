d72adb1c2df569b8309d555ffc1886fe0367d946
Yeluri Nakshatra <nakshatra.yeluri@gmail.com>
2025-01-17 11:02:32 +0530

    asdf

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+export default {
+  content: [
+    "./index.html",       // Include index.html
+    "./src/**/*.{js,ts,jsx,tsx}", // Include all JS/TS/JSX/TSX files in src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
